42391bac8fd8aae5e88f783ca7bfadf9b56f4a5d
Sami Elsayed <2025selsayed@tjhsst.edu>
2022-10-05 11:52:55 -0400

    add styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,18 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const IndexPage = () => {
+	return (
+		<Layout pageTitle = "Home Page">
+			<p>I'm making this by following the Gatsby Tutorial.</p>
+		</Layout>
+	)
+}
+
+export const Head = () => <title>Home page</title>
+
+export default IndexPage
+/*
 import * as React from "react"
 
 const pageStyles = {
@@ -174,3 +189,4 @@ const IndexPage = () => {
 export default IndexPage
 
 export const Head = () => <title>Home Page</title>
+*/
